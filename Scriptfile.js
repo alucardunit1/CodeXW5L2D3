a1f2615293db403b6c8d2c51ee3cdf52772be9a1
alucardunit1 <alucardunit1@gmail.com>
2025-02-28 22:17:23 -0500

    added calculator

--- Scriptfile.js
+++ Scriptfile.js
@@ -1,7 +1,7 @@
 function promptUser() {
-  let userInput = prompt("Please enter a string:");
-  if (userInput !== null) {
-    alert("You entered: " + userInput);
+  let billamt = prompt("Please enter your total bill:");
+  let ppl = prompt("Please enter the number of people:");
+  if (billamt !== null) {
+    alert("Your split bill is: " + billamt / ppl);
   }
 }
-
